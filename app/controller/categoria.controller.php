56692c8867a0884f59bede7e60d2b1da1a043e46
Georgina suarez <suarezgeoor@gmail.com>
2024-11-17 23:35:04 -0300

    Agrego funciones de categoria

---
+++ app/controller/categoria.controller.php
@@ -0,0 +1,68 @@
+<?php
+require_once './app/model/categoria.model.php';
+require_once './app/view/json.view.php';
+
+class categoriaApiController{
+    private $model;
+    private $view;
+
+    public function __construct(){
+        $this->model = new categoriaModel();
+        $this->view = new JSONView();
+    }
+
+    public function getAll($req, $res){
+        $categorias = $this->model->getCategorias();
+
+        return $this->view->response($categorias);
+    }
+
+    public function get($req, $res){
+        $ID_categoria = $req->params->id;
+
+        $categoria = $this->model->verCategoriaById($ID_categoria);
+        if(!$categoria){
+            return $this->view->response("La categoria con el id= $ID_categoria no existe", 404);
+        }
+        return $this->view->response($categoria);
+    }
+
+    public function delete($req, $res){
+        $ID_categoria = $req->params->id;
+        $categoria = $this->model->getCategorias();
+        if(!$categoria){
+            return $this->view->response("La categoria con el id= $ID_categoria no existe", 404);
+        }
+        $this->model->borrarCategoria($ID_categoria);
+        return $this->view->response("La categoria con el id= $ID_categoria se elimino con exito", 200);
+
+    }
+
+    public function create($req, $res){
+        if(empty($req->body['temporada']) || empty($req->body['empresa']) || empty($req->body['comodidad'])){
+            return $this->view->response("Falta completar los campos", 400);
+        }
+
+        $temporada = $req->body['temporada'];
+        $empresa = $req->body['empresa'];
+        $comodidad = $req->body['comodidad'];
+
+        $ID_categoria = $this->model->agregarCategoria($temporada, $empresa, $comodidad);
+        if(!$ID_categoria){
+            return $this->view->response("La categoria fue creado con éxito con id=$ID_categoria", 201);
+           }else{
+            return $this->view->response("Error al crear la categoria=$ID_categoria", 500);
+           }
+    }
+
+    public function update($req,$res){
+        $ID_categoria = $req->params->id;
+     
+          $categoria = $this->model->getCategorias();
+     
+          if(!$categoria){
+           return $this->view->response("La categoria con el id= $ID_categoria no existe" , 404);
+          }
+       }
+}
+?>
--- router.php
+++ router.php
@@ -1,6 +1,7 @@
 <?php 
  require_once './libs/router.php';
 require_once './app/controller/viaje.controller.php';
+require_once './app/controller/categoria.controller.php';
  //Incluyo controllers
 
 $router = new Router();
@@ -11,6 +12,11 @@ $router->addRoute('viajes/:id', 'GET', 'viajeApiController', 'get');
 $router->addRoute('viajes/:id', 'DELETE', 'viajeApiController', 'delete');
 $router->addRoute('viajes', 'POST', 'viajeApiController', 'create');
 $router->addRoute('viajes/:id', 'PUT', 'viajeApiController', 'update');
+$router->addRoute('categorias',  'GET', 'categoriaApiController', 'getAll');
+$router->addRoute('categorias/:id', 'GET', 'categoriaApiController', 'get');
+$router->addRoute('categorias/:id', 'DELETE', 'categoriaApiController', 'delete');
+$router->addRoute('categorias', 'POST', 'categoriaApiController', 'create');
+$router->addRoute('categorias/:id', 'PUT', 'categoriaApiController', 'update');
 
 $router->route($_GET['resource'],$_SERVER ['REQUEST_METHOD']);
 ?>
